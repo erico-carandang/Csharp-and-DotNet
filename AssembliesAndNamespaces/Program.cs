3fa173c633bbb3890d8fdd33e6976ae934944eb1
Edwin Rico Carandang <edwinrn.carandang@yahoo.com>
2021-06-26 17:12:28 -0500

    Add AssembliesAndNamespaces and DotNetCoreeverywhere

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+
+            string s1 = "Hello";
+            String s2 = "World";
+        }
+    }
+}
